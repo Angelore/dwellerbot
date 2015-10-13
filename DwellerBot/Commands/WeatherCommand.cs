2148d2045cd3ab83940401d0c0626df4a415e05c
Stanislau <angelore.main@gmail.com>
2015-10-13 10:28:27 +0300

    Changed temp from range to one value

--- DwellerBot/Commands/WeatherCommand.cs
+++ DwellerBot/Commands/WeatherCommand.cs
@@ -38,9 +38,10 @@ namespace DwellerBot.Commands
             sb.Append("Погода в " + weatherContainer.name + ", " + weatherContainer.sys.country);
             sb.AppendLine(" на " + UnixTimeStampToDateTime(weatherContainer.dt).ToLocalTime().ToShortDateString());
             sb.AppendLine();
-            sb.AppendLine("От " + weatherContainer.main.temp_min +
+            /*sb.AppendLine("От " + weatherContainer.main.temp_min +
                           " до " + weatherContainer.main.temp_max + " *С, " +
-                          weatherContainer.weather[0].description);
+                          weatherContainer.weather[0].description);*/
+            sb.AppendLine("Температура: " + weatherContainer.main.temp + " *C, " + weatherContainer.weather[0].description);
             sb.AppendLine("Влажность: " + weatherContainer.main.humidity + "%");
             sb.AppendLine("Ветер: " + weatherContainer.wind.speed + " м/с");
             await _bot.SendTextMessage(update.Message.Chat.Id, sb.ToString(), false, update.Message.MessageId);
