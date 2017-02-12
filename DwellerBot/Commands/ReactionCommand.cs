ddb1e89d4626f7c0f2e6530d1256d4adce23d097
Stanislau <angelore.main@gmail.com>
2017-02-12 16:58:29 +0300

    Added an option to use "ignorelast" argument with reaction command

--- DwellerBot/Commands/ReactionCommand.cs
+++ DwellerBot/Commands/ReactionCommand.cs
@@ -18,6 +18,8 @@ namespace DwellerBot.Commands
         private readonly Random _rng;
         private readonly string _cacheFilePath;
         private Dictionary<string, string> _sentFiles;
+        private List<string> _ignoredFiles;
+        private string _lastUsedFile;
 
         public ReactionCommand(TelegramBotClient bot, List<string> folderNames, string cacheFilePath):base(bot)
         {
@@ -36,6 +38,8 @@ namespace DwellerBot.Commands
             // Since Telegram allows you to "send" files by using their id (if they are on the server already),
             // I use this to create a simple cache by sending id of a file if it was already sent once.
             _sentFiles = new Dictionary<string, string>();
+            _ignoredFiles = new List<string>();
+            _lastUsedFile = null;
         }
 
         public override async Task ExecuteAsync(Update update, Dictionary<string, string> parsedMessage)
@@ -47,7 +51,53 @@ namespace DwellerBot.Commands
                 return;
             }
 
-            var ind = _rng.Next(0, _files.Count);
+            if (parsedMessage.ContainsKey("message") && !string.IsNullOrEmpty(parsedMessage["message"]))
+            {
+                if (parsedMessage["message"] == "ignorelast")
+                {
+                    if(DwellerBot.IsUserOwner(update.Message.From))
+                    {
+                        if (!string.IsNullOrEmpty(_lastUsedFile))
+                        {
+                            if (!_sentFiles.ContainsKey(_lastUsedFile))
+                                Log.Logger.Debug("_lastUsedFile is not null, but is absent from _sentFiles!");
+                            else
+                                _sentFiles.Remove(_lastUsedFile);
+
+                            if (_ignoredFiles.Contains(_lastUsedFile))
+                                Log.Logger.Debug("Last file was already in the _ignoredFiles.");
+                            else
+                                _ignoredFiles.Add(_lastUsedFile);
+
+                            _lastUsedFile = null;
+                            await Bot.SendTextMessageAsync(update.Message.Chat.Id, "Last sent file has been added to ignored files.", false, false, update.Message.MessageId);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        await Bot.SendTextMessageAsync(update.Message.Chat.Id, "Only bot owner can use this command.", false, false, update.Message.MessageId);
+                        return;
+                    }
+                    return;
+                }
+                // add more handles here if needed
+                else
+                {
+                    await Bot.SendTextMessageAsync(update.Message.Chat.Id, "Unrecognized arguments", false, false, update.Message.MessageId);
+                    return;
+                }
+            }
+
+            int ind;
+            do
+            {
+                ind = _rng.Next(0, _files.Count);
+            }
+            while (!_ignoredFiles.Contains(_files[ind].FullName));
+
+            var previousUsedFile = _lastUsedFile;
+            _lastUsedFile = _files[ind].FullName;
             if (_sentFiles.ContainsKey(_files[ind].FullName))
             {
                 try
@@ -56,12 +106,14 @@ namespace DwellerBot.Commands
                     // which is not the case if you use an image from telegram servers, so this better stay deactivated.
                     // await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.UploadPhoto);
                     await Bot.SendPhotoAsync(update.Message.Chat.Id, _sentFiles[_files[ind].FullName], "", false, update.Message.MessageId);
+                    return;
                 }
                 catch (Exception ex)
                 {
                     Log.Logger.Error("An error has occured during file resending! Error message: {0}", ex.Message);
+                    // remove the erroneous entry and try again
+                    _sentFiles.Remove(_files[ind].FullName);
                 }
-                return;
             }
 
             using (var fs = new FileStream(_files[ind].FullName, FileMode.Open, FileAccess.Read))
@@ -69,7 +121,7 @@ namespace DwellerBot.Commands
                 try
                 {
                     await Bot.SendChatActionAsync(update.Message.Chat.Id, ChatAction.UploadPhoto);
-                    var message = await Bot.SendPhotoAsync(update.Message.Chat.Id, new FileToSend(_files[ind].Name, fs ), "", false, update.Message.MessageId);
+                    var message = await Bot.SendPhotoAsync(update.Message.Chat.Id, new FileToSend(_files[ind].Name, fs), "", false, update.Message.MessageId);
                     lock (_files)
                     {
                         _sentFiles.Add(_files[ind].FullName, message.Photo.Last().FileId);
@@ -78,6 +130,7 @@ namespace DwellerBot.Commands
                 catch (Exception ex)
                 {
                     Log.Logger.Error(ex, "An error has occured during file sending! Error message: {0} File name: {1}", ex.Message, _files[ind].FullName);
+                    _lastUsedFile = previousUsedFile;
                 }
             }
         }
@@ -89,8 +142,9 @@ namespace DwellerBot.Commands
 
             using (var sw = new StreamWriter(new FileStream(_cacheFilePath, FileMode.Create, FileAccess.Write)))
             {
-                var config = JsonConvert.SerializeObject(_sentFiles, Formatting.Indented);
-                sw.WriteLine(config);
+                var config = new ReactionImageCache() { ValidPaths = _sentFiles, IgnoredPaths = _ignoredFiles };
+                var contents = JsonConvert.SerializeObject(config, Formatting.Indented);
+                sw.WriteLine(contents);
             }
             
             Log.Logger.Debug("ReactionCommand state was successfully saved.");
@@ -103,17 +157,20 @@ namespace DwellerBot.Commands
                 var str = sr.ReadToEnd();
                 if (str.Length > 0)
                 {
-                    Dictionary<string, string> config = null;
+                    ReactionImageCache config = null;
                     try
                     {
-                        config = JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
+                        config = JsonConvert.DeserializeObject<ReactionImageCache>(str);
                     }
                     catch (Exception ex)
                     {
                         Log.Logger.Error("An error as occured during parsing of {0} file. Error message: {1}", _cacheFilePath, ex.Message);
                     }
                     if (config != null)
-                        _sentFiles = config;
+                    {
+                        _sentFiles = config.ValidPaths;
+                        _ignoredFiles = config.IgnoredPaths;
+                    }
                 }
                 else
                 {
@@ -121,5 +178,11 @@ namespace DwellerBot.Commands
                 }
             }
         }
+    }
+
+    class ReactionImageCache
+    {
+        public Dictionary<string, string> ValidPaths { get; set; }
+        public List<string> IgnoredPaths { get; set; }
     }
 }
