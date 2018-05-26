fd75b3b02580f74893dc1b5c30fa18a0008285de
Angelore <angelore.main@gmail.com>
2018-05-26 18:35:33 +0300

    Added new libraries in the publish script

--- DwellerBot/publish.bat
+++ DwellerBot/publish.bat
@@ -8,10 +8,16 @@ cd bin\debug\
 copy /Y DwellerBot.exe %publishpath%
 copy /Y DwellerBot.exe.config %publishpath%
 copy /Y HtmlAgilityPack.dll %publishpath%
+copy /Y Microsoft.Recognizers.Definitions.dll %publishpath%
+copy /Y Microsoft.Recognizers.Text.dll %publishpath%
+copy /Y Microsoft.Recognizers.Text.Number.dll %publishpath%
+copy /Y Microsoft.Recognizers.Text.NumberWithUnit.dll %publishpath%
 copy /Y Newtonsoft.Json.dll %publishpath%
 copy /Y Serilog.dll %publishpath%
 copy /Y Serilog.FullNetFx.dll %publishpath%
 copy /Y Serilog.Sinks.ColoredConsole.dll %publishpath%
+copy /Y System.Collections.Immutable.dll %publishpath%
+copy /Y System.ValueTuple.dll %publishpath%
 copy /Y System.Net.Http.Extensions.dll %publishpath%
 copy /Y System.Net.Http.Formatting.dll %publishpath%
 copy /Y System.Net.Http.Primitives.dll %publishpath%
