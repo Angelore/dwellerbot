0e762d7c2289243d58aa8648762d4c5720e4c898
Angelore <angelore.main@gmail.com>
2020-03-08 20:04:28 +0300

    Never miss a dll again! Buy now

--- DwellerBot/publish.bat
+++ DwellerBot/publish.bat
@@ -7,23 +7,8 @@ cd bin\debug\
 @echo on
 copy /Y DwellerBot.exe %publishpath%
 copy /Y DwellerBot.exe.config %publishpath%
-copy /Y HtmlAgilityPack.dll %publishpath%
-copy /Y Microsoft.Recognizers.Definitions.dll %publishpath%
-copy /Y Microsoft.Recognizers.Text.dll %publishpath%
-copy /Y Microsoft.Recognizers.Text.Number.dll %publishpath%
-copy /Y Microsoft.Recognizers.Text.NumberWithUnit.dll %publishpath%
-copy /Y Newtonsoft.Json.dll %publishpath%
-copy /Y Serilog.dll %publishpath%
-copy /Y Serilog.FullNetFx.dll %publishpath%
-copy /Y Serilog.Sinks.Console.dll %publishpath%
-copy /Y Serilog.Sinks.ColoredConsole.dll %publishpath%
-copy /Y System.Collections.Immutable.dll %publishpath%
-copy /Y System.ValueTuple.dll %publishpath%
-copy /Y System.Net.Http.Extensions.dll %publishpath%
-copy /Y System.Net.Http.Formatting.dll %publishpath%
-copy /Y System.Net.Http.Primitives.dll %publishpath%
-copy /Y Telegram.Bot.dll %publishpath%
 copy /Y Resources\changelog.json %publishpath%\Resources
+xcopy /y /d  "*.dll" %publishpath%
 @echo off
 echo Done
 
